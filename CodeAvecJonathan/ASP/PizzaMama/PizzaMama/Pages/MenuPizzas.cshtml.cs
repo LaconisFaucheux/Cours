c1228bdd1f3954283bb72beb557cb268f167ecf6
LaconisFaucheux <nicolas.frotey@gmail.com>
2023-08-03 12:45:22 +0200

    commit changement de PC

--- CodeAvecJonathan/ASP/PizzaMama/PizzaMama/Pages/MenuPizzas.cshtml.cs
+++ CodeAvecJonathan/ASP/PizzaMama/PizzaMama/Pages/MenuPizzas.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PizzaMama.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace PizzaMama.Pages
 {
     public class MenuPizzasModel : PageModel
     {
-        public void OnGet()
+        private readonly PizzaMama.Data.DataContext _context;
+        public IList<Pizza> cartePizzas {get; set;}
+
+        public MenuPizzasModel(PizzaMama.Data.DataContext context)
         {
+            _context = context;
+        }
+        public async Task OnGetAsync()
+        {
+            cartePizzas = await _context.Pizzas.ToListAsync();
         }
     }
 }
